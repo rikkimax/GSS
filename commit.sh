65b3f1734842fa00be954de98b04e0445b773462
rikki molecatert <pbdadmin@gmail.com>
2011-09-08 23:10:25 +1200

    Removed some unneeded files

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo -n "Please tell me the commit message: "
-read -e message
-MSG="'${message}'"
-git add config
-git add src
-COMMIT="git commit -m ${MSG}"
-echo $COMMIT
-git push origin master
